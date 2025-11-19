34cbbc6243df2664a9e258279982e5c98e9b1284
2S-Global <2sglobaltechnologies@gmail.com>
2025-11-19 12:32:39 +0530

    update

--- components/common/form/login/FormContent2.jsx
+++ components/common/form/login/FormContent2.jsx
@@ -99,7 +99,7 @@ const FormContent2 = () => {
       <div className="mb-3 d-flex justify-content-center pb-4 pt-4">
         <img
           alt="brand"
-          src="/images/FoodvGo.png"
+          src="/images/FoodGo.png"
           /* width={200}
           height={60} */
           /*   style={{ height: "200px" }} */
