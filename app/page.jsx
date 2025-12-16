bae9d1e0b4d2cda4e4f5c285c582271b63882ca3
Chandra-Sarkar <chandrasarkar26345@gmail.com>
2025-12-16 10:43:32 +0530

    fix: Update comment for Home component in page.jsx to include author name

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function page() {
   return (
     <Wrapper>
-      {/*   <Home /> */}
+      {/*   <Home /> Chandra*/}
       <LogIn />
     </Wrapper>
   );
