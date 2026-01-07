e6ebc7abaed0f1657dd7558080ffcc7417bb5683
2S-Global <2sglobaltechnologies@gmail.com>
2026-01-07 16:37:21 +0530

    Add files via upload

---
+++ app/admin/item-wise-statistics/page.jsx
@@ -0,0 +1,6 @@
+import ItemWiseStatistics from "@/components/admin/admin-dashboard/item-wise-statistics";
+
+
+export default function ItemWiseStatisticsPage() {
+  return <ItemWiseStatistics />;
+}
