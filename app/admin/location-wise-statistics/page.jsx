5da334fd880cb91f168355e31955fac5898ef673
2S-Global <2sglobaltechnologies@gmail.com>
2026-01-07 16:38:02 +0530

    Add files via upload

---
+++ app/admin/location-wise-statistics/page.jsx
@@ -0,0 +1,5 @@
+import LocationWiseStatistics from "@/components/admin/admin-dashboard/location-wise-statistics";
+
+export default function LocationWiseStatisticsPage() {
+  return <LocationWiseStatistics />;
+}
